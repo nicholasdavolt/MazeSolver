f3141f856dfab6eacf84e1ad49accf04b84a345b
nicholasdavolt <nicholasdavolt@gmail.com>
2024-02-02 15:20:51 -0600

    Created point and line classes, draw line function

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from tkinter import Tk, BOTH, Canvas
 import time
 def main():
     win = Window(800, 600)
+    p1 = Point(0,0)
+    p2 = Point(800, 600)
+    line1 = Line(p1, p2)
+    line2 = Line(Point(0, 600), Point(800, 0))
+
+    win.draw_line(line1,"red")
+    win.draw_line(line2,"red")
     win.wait_for_close()
-    time.sleep(10)
-    win.close()
+    
 
 class Window:
     def __init__(self, width, height):
@@ -12,10 +18,14 @@ class Window:
         self.height = height
         self.root = Tk()
         self.root.title = "MAZE SOLVER"
-        self.canvas = Canvas()
-        self.canvas.pack()
-        self.running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
+        self.canvas = Canvas(self.root, bg="white", height=self.height, width=self.width)
+        self.canvas.pack(fill=BOTH, expand=1)
+        self.running = False
+        
+
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
 
     def redraw(self):
         self.root.update_idletasks()
@@ -28,4 +38,24 @@ class Window:
 
     def close(self):
         self.running = False
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.point_1 = p1
+        self.point_2 = p2
+
+    def draw(self, canvas, fill_color):
+        x1 = self.point_1.x
+        y1 = self.point_1.y
+        x2 = self.point_2.x
+        y2 = self.point_2.y
+
+        canvas.create_line(x1, y1, x2, y2, fill=fill_color, width=2)
+        canvas.pack(fill=BOTH, expand=1)
+        
 main()
